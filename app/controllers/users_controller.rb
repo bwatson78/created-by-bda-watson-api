353acddcf71103b2f8312c108a8cb0e4206bbea6
bwatson78 <brad.watson.orlando@gmail.com>
2019-11-25 08:31:29 -0600

    Gets the tests passing for the authorization processes for users/:id.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,12 +5,16 @@ class UsersController < ApplicationController
   before_action :authorize_request, only: :show
 
   def show
-    @user = User.find(params[:id])
-    render json: @user.as_json.merge(social_sites: @user.social_sites.as_json)
+    @user = User.find_by_id(params[:id])
+    if @user
+      render json: UserSerializer.new(@user).as_json
+    else
+      render json: {error: 'User not found.', status: 401}, status: 401
+    end
   end
 
   def brads_deets
-    @user = User.find_by_email('brad.watson.orlando@gmail.com')
-    render json: UserSerializer.new(@user).as_json
+    @user = User.find_by_email(ENV['BRADS_EMAIL'])
+    render json: BradDeetsSerializer.new(@user).as_json
   end
 end
